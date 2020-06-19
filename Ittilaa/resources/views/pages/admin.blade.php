872f7e188ecb3fcf379461c49f3609963f8868a8
Myra Zafar <MyraZafar@ad.lmkr.net>
2020-06-19 05:49:51 +0500

    Admin UI

--- Ittilaa/resources/views/pages/admin.blade.php
+++ Ittilaa/resources/views/pages/admin.blade.php
@@ -10,8 +10,91 @@
 @section('content')
 
 <!-- add HTML related to login here -->
-<H1>
-    Admin page
-</H1>
+<div class="container-fluid row">
+   <div class="col-md-3">
+      <div class="sidenav">
+         <a href="#">Pending Notifications</a>
+         <a href="#">Approved Notifications</a>
+         <a href="#">Rejected Notifications</a>
+         <a href="#">Data Entry View</a>
+      </div>
+   </div>
+   <div class="col-md-8">
+       <div class="row page-heading">
+           <h3>Pending Notifications</h3>
+       </div>
+       <div class="row admin-notifs">
+          <div class="card admin-card col-md-12">
+              <div class="col-md-2">
+                 <a href=#><img class="card-img-top" src="../images/Card test2.png" alt="Card image"></a>
+              </div>
+              <div class="card-body admin-card-body col-md-9">
+                 <h4 class="card-title">Notification Title</h4>
+                 <p class="card-text">Date: 1st January 2020</p>
+                 <p class="card-text">Tags: Tag1, Tag2, Tag3, Tag4, Tag5</p>
+                 <p class="card-text">Department: Ministry of Magic</p>
+                 <a href="#" class="btn btn-success btn-accept">Approve</a>
+                 <a href="#" class="btn btn-danger btn-reject">Reject</a>
+             </div>
+          </div>
+       </div>
+       <div class="row admin-notifs">
+          <div class="card admin-card col-md-12">
+              <div class="col-md-2">
+                 <a href=#><img class="card-img-top" src="../images/Card test2.png" alt="Card image"></a>
+              </div>
+              <div class="card-body admin-card-body col-md-9">
+                 <h4 class="card-title">Notification Title</h4>
+                 <p class="card-text">Date: 1st January 2020</p>
+                 <p class="card-text">Tags: Tag1, Tag2, Tag3, Tag4, Tag5</p>
+                 <p class="card-text">Department: Ministry of Magic</p>
+                 <a href="#" class="btn btn-success btn-accept">Approve</a>
+                 <a href="#" class="btn btn-danger btn-reject">Reject</a>
+             </div>
+          </div>
+       </div>
+       <div class="row admin-notifs">
+          <div class="card admin-card col-md-12">
+              <div class="col-md-2">
+                 <a href=#><img class="card-img-top" src="../images/Card test2.png" alt="Card image"></a>
+              </div>
+              <div class="card-body admin-card-body col-md-9">
+                 <h4 class="card-title">Notification Title</h4>
+                 <p class="card-text">Date: 1st January 2020</p>
+                 <p class="card-text">Tags: Tag1, Tag2, Tag3, Tag4, Tag5</p>
+                 <p class="card-text">Department: Ministry of Magic</p>
+                 <a href="#" class="btn btn-success btn-accept">Approve</a>
+                 <a href="#" class="btn btn-danger btn-reject">Reject</a>
+             </div>
+          </div>
+       </div>
+       <div class="row admin-notifs">
+          <div class="card admin-card col-md-12">
+              <div class="col-md-2">
+                 <a href=#><img class="card-img-top" src="../images/Card test2.png" alt="Card image"></a>
+              </div>
+              <div class="card-body admin-card-body col-md-9">
+                 <h4 class="card-title">Notification Title</h4>
+                 <p class="card-text">Date: 1st January 2020</p>
+                 <p class="card-text">Tags: Tag1, Tag2, Tag3, Tag4, Tag5</p>
+                 <p class="card-text">Department: Ministry of Magic</p>
+                 <a href="#" class="btn btn-success btn-accept">Approve</a>
+                 <a href="#" class="btn btn-danger btn-reject">Reject</a>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+       <div class="col-md-3">
+          <ul class="pagination custom-pagination">
+             <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+             <li class="page-item"><a class="page-link" href="#">1</a></li>
+             <li class="page-item"><a class="page-link" href="#">2</a></li>
+             <li class="page-item"><a class="page-link" href="#">3</a></li>
+             <li class="page-item"><a class="page-link" href="#">Next</a></li>
+         </ul> 
+      </div>
+</div>
+   </div>
+</div>
 
 @endsection
