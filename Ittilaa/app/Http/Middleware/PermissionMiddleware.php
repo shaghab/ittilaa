2b85618fd7aa5e51678d1c0f3ab8a02d52bb7230
nosheennisar <nosheen.nisar@gmail.com>
2020-07-21 04:28:33 +0500

    Corrected login routes

--- Ittilaa/app/Http/Middleware/PermissionMiddleware.php
+++ Ittilaa/app/Http/Middleware/PermissionMiddleware.php
@@ -17,7 +17,19 @@ class PermissionMiddleware
      */
     public function handle($request, Closure $next, $permission)
     {
+        // TODO: later add guest user in session
         if ($request->user() === null) {
+            // Get URLs
+            $urlCurrent = url()->current();
+            $urlBase = url()->to('/');
+
+            // Set the previous url that we came from to redirect to after successful login but only if is internal
+            if (($urlCurrent != $urlBase.RouteServiceProvider::LOGIN &&
+                $urlCurrent != $urlBase.RouteServiceProvider::REGISTER) && 
+                (substr($urlCurrent, 0, strlen($urlBase)) === $urlBase))    {
+                session()->put('url.intended', $urlCurrent);
+            }
+
             return redirect(RouteServiceProvider::LOGIN);
         }
 
