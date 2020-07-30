6391052e07d03ba488bcee4889dc4b6fe6c7a507
nosheennisar <nosheen.nisar@gmail.com>
2020-07-30 20:34:19 +0500

    Home page responsiveness

--- Ittilaa/resources/views/includes/footer.blade.php
+++ Ittilaa/resources/views/includes/footer.blade.php
@@ -5,20 +5,20 @@
       <hr/>
       <div class="col-md-12 footer-content">
           <div class="col-md-1"></div>
-          <div class="col-md-5">
+          <div class="col-md-4 col-lg-5">
              <img class="footer-logo" src="../images/ICT Logo_001.png" alt="ICT Logo">
              <p>Ittilaa about text. Ittilaa about text. Ittilaa about text. 
                  Ittilaa about text. Ittilaa about text. Ittilaa about text. 
                  Ittilaa about text. Ittilaa about text. Ittilaa about text.
                  Ittilaa about text. Ittilaa about text. Ittilaa about text. </p>
           </div>
-          <div class="col-md-1"></div>
-          <div class="col-md-2">
+          <div class="d-none d-lg-block col-lg-1"></div>
+          <div class="col-md-3 col-lg-2">
               <h6>Contact Us</h6>
               <p>0092-51-1234567</p>
               <p>ICT4Good@email.com</p>
           </div>
-          <div class="col-md-2 sm-icons">
+          <div class="col-md-3 col-lg-2 sm-icons">
               <h6>Follow Us</h6>
               <a href=#><span class="fa fa-facebook-square"></span></a>
               <a href=#><span class="fa fa-twitter-square"></span></a>
--- Ittilaa/resources/views/pages/home.blade.php
+++ Ittilaa/resources/views/pages/home.blade.php
@@ -3,23 +3,28 @@
 @extends('layouts.default')
 @section('content')
 
-<div class="container-fluid, page-heading row">
-       <h3>CONVENIENT ACCESS TO GOVERNMENT NOTICES AND POLICIES</h3>
+<div class="container page-heading">
+       <h3 class="text-center">CONVENIENT ACCESS TO GOVERNMENT NOTICES AND POLICIES</h3>
        <p>Don't miss out on the notices and policies that are important to you. Use Ittilaa to explore verified Government notifications.</p>
 </div>
 
-<div class="row">
-  <form class="form-inline col-sm-6">
+<div class="container-fluid search">
+  <form class="form-inline col-lg-12 d-none d-lg-flex">
      <div class="form-group search-container">
-         <span class="fa fa-search form-control-icon"></span>
+         {{-- <span class="fa fa-search form-control-icon"></span> --}}
          <input type="text" class="form-control" placeholder="Find any public notification..." name="search">
-         <button type="submit" class="btn btn-primary btn-main">Search</button>
+         <button type="submit" class="btn btn-primary search-btn"><i class="fa fa-search"></i>Search</button>
      </div>
   </form>
+  <form class="form-inline col-xs-12 d-flex d-lg-none">
+   <div class="form-group search-container col">
+       <input type="text" class="form-control" placeholder="Find any public notification..." name="search">
+       <button type="submit" class="btn btn-primary search-btn"><i class="fa fa-search"></i></button>
+</form>
 </div>
-<div class="row">
-   <div class="col-sm-6 filters">
-      <div class="dropdown">
+<div class="container filters-container">
+   <div class="col filters d-none d-md-flex">
+      <div class="dropdown col">
          <label class="">Filters:</label>
          <form class="" method="POST" action="{{ route('search_region') }}" enctype="multipart/form-data" >
          {{ csrf_field() }}
@@ -86,20 +91,84 @@
          </form>
       </div>
    </div>
-</div>
-<div class="row">
-   <div class="col-sm-9 section-header">
-      <h6>MOST RECENT</h6>
-   </div>
-</div>
+   <div class="col filters d-inline d-md-none">
+         <label class="col">Filters</label>
+         <form class="col" method="POST" action="{{ route('search_region') }}" enctype="multipart/form-data" >
+         {{ csrf_field() }}
+         
+            <div class="dropdown region-dropdown col">
+               <select id="region_id" name="region_id" class="form-control" onchange="this.form.submit()">
+                  <div class="dropdown-menu">
+                     <option class="dropdown-item" value="">Region</option>
 
+                     @foreach ($regions as $region)
+                        <option class="dropdown-item" value="{{ $region->name }}">{{ $region->name }}</option>
+                     </option>
+                     @endforeach
+                  </div>
+               </select>
+
+               @if ($errors->has('region_id'))
+                  <span class="help-block">
+                     <strong>{{ $errors->first('region_id') }}</strong>
+                  </span>
+               @endif
+            </div>
+         </form> 
+         <form class="col" method="POST" action="{{ route('search_department') }}" enctype="multipart/form-data" >
+            {{ csrf_field() }}
+
+            <div class="dropdown department-dropdown col">
+               <select id="department" name="department" class="form-control" onchange="this.form.submit()">
+                  <div class="dropdown-menu">
+                     <option class="dropdown-item" value="">Department</option>
+
+                     @foreach ($departments as $department)
+                        <option class="dropdown-item" value="{{ $department->unit_name }}">{{ $department->unit_name }}</option>
+                     @endforeach
+                  </div>
+               </select>
+
+               @if ($errors->has('department'))
+                  <span class="help-block">
+                     <strong>{{ $errors->first('department') }}</strong>
+                  </span>
+               @endif
+            </div> 
+         </form>
+         <form class="col" method="POST" action="{{ route('search_category') }}" enctype="multipart/form-data" >
+            {{ csrf_field() }}
+            
+            <div class="dropdown category-dropdown col">
+               <select name="category_id" class="form-control" onchange="this.form.submit()">
+                  <div class="dropdown-menu">
+                     <option class="dropdown-item" value="">Category</option>
+
+                     @foreach ($categories as $category)
+                        <option class="dropdown-item" value="{{ $category['id'] }}"  @if ($category['id'] == value('category_id')) selected @endif>{{ $category['name'] }}</option>
+                     @endforeach
+                  </div>
+               </select>
+
+               @if ($errors->has('category_id'))
+                  <span class="help-block">
+                     <strong>{{ $errors->first('category_id') }}</strong>
+                  </span>
+               @endif
+            </div>
+         </form>
+</div>
+<div class="col-xs-12 section-header">
+   <h6>MOST RECENT</h6>
+</div>
 @if ($count == 0)
-   <div><p>No results found.</p></div>
-@else
-
+<div>
+   <p>No results found.</p>
+</div>
+@else 
    @for ($index = 0, $row = 0; $row < config('pagination.home.number_of_rows'); $row++)
-   <div class="row notifs">
-      <div class="col-xs-8 notifs-row">
+   <div class="container notifs">
+      <div class="col-xs-9 notifs-row">
 
          @for ($col = 0; $col < config('pagination.home.records_per_row') && $index < $count; $col++, $index++)
          <?php $notification = $notifications[$index]; ?>
@@ -107,10 +176,10 @@
          <div class="card col-xs-7 col-sm-4 col-md-2 notifs-card">
             <img class="card-img-top" src="{{ asset($notification->thumbnail_link) }}" alt="Notification image">
             <div class="card-body">
-               <p class="card-text">{{ $notification->caption1 }}</p>
+               <p class="card-text caption1">{{ $notification->caption1 }}</p>
                <h6 class="card-title">{{ $notification->short_title }}
                </h6>
-               <p class="card-text">{{ $notification->caption2 }}</p>
+               <p class="card-text caption2">{{ $notification->caption2 }}</p>
                <p class="card-text">{{ $notification->caption3 }}</p>
             </div>
             <div class="card-img-overlay notifs-img-overlay">
@@ -124,8 +193,8 @@
    @endfor
 @endif
 
-<div class="row">
-      <div class="col-md-3">
+<div class="container">
+      <div class="col-xs-12">
             {{ $notifications->onEachSide(1)->links() }}
 
             {{-- <ul class="pagination custom-pagination">
