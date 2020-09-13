db51b87c283044a2387e318af9d123983e361496
nosheennisar <nosheen.nisar@gmail.com>
2020-09-13 21:01:09 +0500

    Header updated

--- Ittilaa/resources/views/includes/header.blade.php
+++ Ittilaa/resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <!-- This is website header where icons and such are added -->
 
 
-<div class="header-hr">
+<div class="blkheader-hr">
     <span>
        <a href="{{route("home")}}"> <img src="../images/Ittilaa Logo_001.png" alt="Ittilaa Logo"></a>
     </span>
